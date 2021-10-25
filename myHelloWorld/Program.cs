84913df6b2e2b23879165c3c42c588d832e6a7d8
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-25 18:18:11 -0400

    added Console.WriteLine to display current time

--- myHelloWorld/Program.cs
+++ myHelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace myHelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
         }
     }
 }
