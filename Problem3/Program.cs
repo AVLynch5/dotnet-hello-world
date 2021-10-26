6e943b14f68bc8dbce08b872caa2670c5b902deb
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-25 23:45:21 -0400

    copied solution 3 into Problem3

---
+++ Problem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem3
+{
+    public class Kata
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            int totalSum = 0;
+            foreach (int i in arr)
+            {
+                if (i >= 0)
+                {
+                    totalSum += i;
+                }
+            }
+            return totalSum;
+        }
+    }
+
+
+}
