c536e67a1149939d08d73f3c7fc5dcc072248072
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-25 23:39:02 -0400

    created Problem1, Problem2, and Problem3 apps. Added problem 1 code to Problem1

---
+++ Problem1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Problem1
+{
+    public class LoveDetector
+    {
+        public static bool lovefunc(int flower1, int flower2) => ((flower1 % 2 == 0 && flower2 % 2 != 0) || (flower2 % 2 == 0 && flower1 % 2 != 0)) ? true : false;
+    }
+}
