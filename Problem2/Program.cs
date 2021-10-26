a6929c276aaa5d6f3cdf713c3c07fc7beeef111b
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-25 23:42:27 -0400

    copied Problem 2 into Problem2

---
+++ Problem2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem2
+{
+    public class Kata
+    {
+        public static bool Check(object[] a, object x)
+        {
+            foreach (object i in a)
+            {
+                if (i.Equals(x))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
